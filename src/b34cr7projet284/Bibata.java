69d5144d34c71781cbd7b728db685accbec780dc
Bibata Amidou <aa_bibata1@yahoo.ca>
2018-03-01 15:46:52 -0500

    Ajouter constructeur et set/get

--- src/b34cr7projet284/Bibata.java
+++ src/b34cr7projet284/Bibata.java
@@ -12,6 +12,29 @@ package b34cr7projet284;
 public class Bibata {
     private String nom;
     private String prenom;
+
+    public Bibata(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    
     
     
     
